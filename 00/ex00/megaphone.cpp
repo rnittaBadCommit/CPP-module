ddbb923433f9a74c169a2e4c844df9a7f28bd8d4
rnitta <ryushironitta@yahoo.co.jp>
2022-06-21 21:48:54 +0900

    maybe ok

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		for (int i = 1; i < argc; i++)
+			std::cout << argv[i];
+	std::cout << std::endl;
+	return (0);
+}
