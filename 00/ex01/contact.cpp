ff51d0294d1d2c1e09a3e9df7eae8935406dfa94
rnitta <ryushironitta@yahoo.co.jp>
2022-06-21 22:47:01 +0900

    implement contact class

---
+++ 00/ex01/contact.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include "contact.hpp"
+
+Contact::Contact()
+{
+}
+
+Contact::~Contact()
+{
+}
+
+const std::string &Contact::get_first_name()
+{
+	return (_first_name);
+}
+
+const std::string &Contact::get_last_name()
+{
+	return (_last_name);
+}
+
+const std::string &Contact::get_nick_name()
+{
+	return (_nick_name);
+}
+
+void	Contact::set_first_name(const std::string first_name)
+{
+	_first_name = first_name;
+}
+
+void	Contact::set_last_name(const std::string last_name)
+{
+	_last_name = last_name;
+}
+
+void	Contact::set_nick_name(const std::string nick_name)
+{
+	_nick_name = nick_name;
+}
---
+++ 00/ex01/contact.hpp
@@ -0,0 +1,24 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+
+class Contact
+{
+	public:
+		Contact();
+		~Contact();
+		void	set_first_name(const std::string first_name);
+		void	set_last_name(const std::string last_name);
+		void	set_nick_name(const std::string nici_name);
+		const std::string &get_first_name();
+		const std::string &get_last_name();
+		const std::string &get_nick_name();
+
+	private:
+		std::string	_first_name;
+		std::string	_last_name;
+		std::string	_nick_name;	
+}
+
+#endif
