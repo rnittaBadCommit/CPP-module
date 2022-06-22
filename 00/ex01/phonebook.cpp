b4d81fc1713284b54f569c6d9243d4cdd8ec634f
rnitta <ryushironitta@yahoo.co.jp>
2022-06-22 16:25:01 +0900

    need to check phone_number is valid in command_add()

--- 00/ex01/phonebook.cpp
+++ 00/ex01/phonebook.cpp
@@ -1,6 +1,6 @@
 #include "phonebook.hpp"
 
-PhoneBook::PhoneBook() : _num_contact(0), _oldest_contact_index(0)
+PhoneBook::PhoneBook() : _is_shell_finished(false), _num_contact(0), _oldest_contact_index(0)
 {
 
 }
@@ -10,7 +10,45 @@ PhoneBook::~PhoneBook()
 
 }
 
-void	PhoneBook::add_new_contact(const t_contact_info contact_info)
+void	PhoneBook::start_shell()
+{
+	std::string	command;
+
+	while (!_is_shell_finished)
+	{
+		std::cout << "%>" << std::endl;
+		std::getline(std::cin, command);
+		if (_is_valid_command(command))
+			_execute_command(command);
+		else
+			;
+	}
+}
+
+
+//private
+
+bool	PhoneBook::_is_valid_command(const std::string command) const
+{
+	if (command == "")
+		return (false);
+	for (int i = 0; !command_arry[i].empty(); i++)
+		if (command_arry[i] == command)
+			return (true);
+	return (false);
+}
+
+void	PhoneBook::_execute_command(const std::string command)
+{
+	if (command == "ADD")
+		_command_add();
+	else if (command == "SEARCH")
+		_command_search();
+	else if (command == "EXIT")
+		_command_exit();
+}
+
+void	PhoneBook::_add_new_contact(const t_contact_info contact_info)
 {
 	if (_num_contact < MAX_CONTACT_NUM)
 	{
@@ -24,19 +62,19 @@ void	PhoneBook::add_new_contact(const t_contact_info contact_info)
 	}
 }
 
-void	PhoneBook::show_contact_all_info(const int contact_index) const
+void	PhoneBook::_show_contact_all_info(const int contact_index) const
 {
 	_contact_arry[(_oldest_contact_index + contact_index) % 8].show_contact_info();
 }
 
-void	PhoneBook::show_contact_basic_info(const Contact contact, const int width) const
+void	PhoneBook::_show_contact_basic_info(const Contact contact, const int width) const
 {
 	std::cout << std::setw(10) << _convert_str_to_limited_width(contact.get_first_name()) << "|";
 	std::cout << std::setw(10) << _convert_str_to_limited_width(contact.get_last_name()) << "|";
 	std::cout << std::setw(10) << _convert_str_to_limited_width(contact.get_nick_name()) << std::endl;
 }
 
-void	PhoneBook::show_all_contact_basic_info(const int width) const
+void	PhoneBook::_show_all_contact_basic_info(const int width) const
 {
 	int	contact_index = _oldest_contact_index;
 	for (int i = 0; i < _num_contact; i++)
@@ -47,7 +85,21 @@ void	PhoneBook::show_all_contact_basic_info(const int width) const
 	}
 }
 
-//private
+void	PhoneBook::_command_add()
+{
+	t_contact_info new_contact_info;
+
+	std::cout << "input first name: ";
+	std::getline(std::cin, new_contact_info.first_name);
+	std::cout << "input last name: ";
+	std::getline(std::cin, new_contact_info.last_name);
+	std::cout << "input nick name: ";
+	std::getline(std::cin, new_contact_info.nick_name);
+	std::cout << "input phone number: ";
+	std::getline(std::cin, new_contact_info.phone_number);
+	std::cout << "input darkest secret: ";
+	std::getline(std::cin, new_contact_info.secret_info);
+}
 
 const std::string PhoneBook::_convert_str_to_limited_width(const std::string src) const
 {
@@ -61,12 +113,3 @@ const std::string PhoneBook::_convert_str_to_limited_width(const std::string src
 	}	
 }
 
-bool	PhoneBook::_is_valid_command(const std::string command) const
-{
-	if (command == "")
-		return (false);
-	for (int i = 0; !command_arry[i].empty(); i++)
-		if (command_arry[i] == command)
-			return (true);
-	return (false);
-}
--- 00/ex01/phonebook.hpp
+++ 00/ex01/phonebook.hpp
@@ -14,20 +14,24 @@ class PhoneBook
 	public:
 		PhoneBook();
 		~PhoneBook();
-		void	add_new_contact(const t_contact_info contact_info);
-		void	show_contact_all_info(const int contact_index) const;
-		void	show_contact_basic_info(const Contact contact, const int width) const;
-		void	show_all_contact_basic_info(const int width) const;
-		bool	is_valid_command(const std::string command) const;
+		void	start_shell();
 
 	private:
+		bool	_is_shell_finished;
 		int	_num_contact;
 		int	_oldest_contact_index;
 		Contact _contact_arry[MAX_CONTACT_NUM];
 		const std::string command_arry[COMMAND_NUM + 1] = {"ADD", "SEARCH", "EXIT", ""};
-
+		bool	_is_valid_command(const std::string command) const;
+		void	_execute_command(const std::string command);
+		void	_add_new_contact(const t_contact_info contact_info);
+		void	_show_contact_all_info(const int contact_index) const;
+		void	_show_contact_basic_info(const Contact contact, const int width) const;
+		void	_show_all_contact_basic_info(const int width) const;
+		void	_command_add();
+		void	_command_search();
+		void	_command_exit();
 		const std::string _convert_str_to_limited_width(const std::string src) const;
-		bool	is_valid_command(const std::string command) const;
 };
 
 #endif
