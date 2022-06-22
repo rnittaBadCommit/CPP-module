1b0d3bc4685bd298d757a07e95cb2b9504ee16e6
rnitta <ryushironitta@yahoo.co.jp>
2022-06-22 13:31:34 +0900

    add other info and show_contact_info()

--- 00/ex01/contact.cpp
+++ 00/ex01/contact.cpp
@@ -4,36 +4,80 @@ Contact::Contact()
 {
 }
 
+Contact::Contact(t_contact_info contact_info)
+{
+	_contact_info = contact_info;
+}
+
 Contact::~Contact()
 {
 }
 
-const std::string &Contact::get_first_name() const
+void	Contact::show_contact_info() const
 {
-	return (_first_name);
-}
-
-const std::string &Contact::get_last_name() const
-{
-	return (_last_name);
-}
-
-const std::string &Contact::get_nick_name() const
-{
-	return (_nick_name);
+	std::cout << "first name: " << _contact_info.first_name << std::endl;
+	std::cout << "last name: " << _contact_info.last_name << std::endl;
+	std::cout << "nick name: " << _contact_info.nick_name << std::endl;
+	std::cout << "phone number: " << _contact_info.phone_number << std::endl;
+	std::cout << "darkest secret: " << _contact_info.secret_info << std::endl;
 }
 
 void	Contact::set_first_name(const std::string first_name)
 {
-	_first_name = first_name;
+	_contact_info.first_name = first_name;
 }
 
 void	Contact::set_last_name(const std::string last_name)
 {
-	_last_name = last_name;
+	_contact_info.last_name = last_name;
 }
 
 void	Contact::set_nick_name(const std::string nick_name)
 {
-	_nick_name = nick_name;
+	_contact_info.nick_name = nick_name;
+}
+
+void	Contact::set_phone_number(const long long int phone_number)
+{
+	_contact_info.phone_number = phone_number;
+}
+
+void	Contact::set_secret_info(const std::string secret_info)
+{
+	_contact_info.secret_info = secret_info;
+}
+
+void	Contact::set_contact_info(const t_contact_info contact_info)
+{
+	_contact_info = contact_info;
+}
+
+const std::string &Contact::get_first_name() const
+{
+	return (_contact_info.first_name);
+}
+
+const std::string &Contact::get_last_name() const
+{
+	return (_contact_info.last_name);
+}
+
+const std::string &Contact::get_nick_name() const
+{
+	return (_contact_info.nick_name);
+}
+
+long long int Contact::get_phone_number() const
+{
+	return (_contact_info.phone_number);
+}
+
+const std::string &Contact::get_secret_info() const
+{
+	return (_contact_info.secret_info);
+}
+
+const t_contact_info &Contact::get_contact_info() const
+{
+	return (_contact_info);
 }
--- 00/ex01/contact.hpp
+++ 00/ex01/contact.hpp
@@ -3,22 +3,37 @@
 
 # include <iostream>
 
+struct t_contact_info
+{
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nick_name;	
+	long long int	phone_number;
+	std::string secret_info;
+};
+
 class Contact
 {
 	public:
 		Contact();
+		Contact(t_contact_info contact_info);
 		virtual ~Contact();
+
+		void	show_contact_info() const;
+
 		void	set_first_name(const std::string first_name);
 		void	set_last_name(const std::string last_name);
-		void	set_nick_name(const std::string nici_name);
+		void	set_nick_name(const std::string nick_name);
+		void	set_phone_number(const long long int phone_number);
+		void	set_secret_info(const std::string secret_info);
 		const std::string &get_first_name() const;
 		const std::string &get_last_name() const;
 		const std::string &get_nick_name() const;
+		long long int get_phone_number() const;
+		const std::string &get_secret_info() const;
 
 	private:
-		std::string	_first_name;
-		std::string	_last_name;
-		std::string	_nick_name;	
-}
+		t_contact_info _contact_info;
+};
 
 #endif
