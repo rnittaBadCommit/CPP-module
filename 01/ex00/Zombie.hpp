e9cdfd1d803e129bd39e9fa53597ddcadce5b5e0
rnitta <ryushironitta@yahoo.co.jp>
2022-06-23 04:03:35 +0900

    recreate

---
+++ 01/ex00/Zombie.hpp
@@ -0,0 +1,21 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie
+{
+	public:
+		Zombie();
+		Zombie(std::string name);
+		virtual  ~Zombie();
+		void	announce(void);
+
+	private:
+		std::string _name;
+};
+
+Zombie* newZombie(std::string name);
+void	randomChump(std::string name);
+
+#endif
