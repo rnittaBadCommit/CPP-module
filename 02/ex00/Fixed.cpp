38bf0da8652a44aa4237fed7b4042eaa04cc229d
rnitta <rnitta@student.42tokyo.jp>
2022-06-24 23:02:42 +0900

    implement Canonical Form

---
+++ 02/ex00/Fixed.cpp
@@ -0,0 +1,22 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : value_(0)
+{
+
+}
+
+Fixed::Fixed(const Fixed &other) : value_(0)
+{
+	*this = other;
+}
+
+Fixed::~Fixed()
+{
+
+}
+
+Fixed& Fixed::operator=(const Fixed &other)
+{
+	this->value_ = other.value_;
+	return (*this);
+}
