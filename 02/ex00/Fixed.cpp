3e7566934dd890ddef630d1917d0189f4286b38f
rnitta <rnitta@student.42tokyo.jp>
2022-06-25 00:35:35 +0900

    same as subject

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -2,31 +2,35 @@
 
 Fixed::Fixed() : value_(0)
 {
-
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &other) : value_(0)
 {
+	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Fixed::~Fixed()
 {
-
+	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(const Fixed &other)
 {
-	this->value_ = other.value_;
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->value_ = other.getRawBits();
 	return (*this);
 }
 
 int	Fixed::getRawBits(void) const
 {
-
+	std::cout << "getRawBits member function called" << std::endl;
+	return (value_);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-
+	std::cout << "setRawBits member function called" << std::endl;
+	value_ = raw;
 }
