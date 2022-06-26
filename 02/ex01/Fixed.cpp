a5e8a19ca1afbcd8bca9bb6d7cd7209253c2c704
rnitta <ryushironitta@yahoo.co.jp>
2022-06-27 01:40:03 +0900

    same output as subject

---
+++ 02/ex01/Fixed.cpp
@@ -0,0 +1,62 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : value_(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) : value_(0)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::Fixed(const int value) : value_(value << fractional_bit_)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value) : value_(roundf(value * (1 << fractional_bit_)))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->value_ = other.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (value_);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	value_ = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)value_ / (1 << fractional_bit_));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (value_ >> fractional_bit_);
+}
+
+std::ostream	&operator<<(std::ostream &stdout, const Fixed &value)
+{
+	stdout << value.toFloat();
+	return (stdout);
+}
---
+++ 02/ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed(const int value);
+		Fixed(const float value);
+		virtual ~Fixed();
+		Fixed	&operator=(const Fixed &other);
+		int	getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+	private:
+		int	value_;
+		static const int fractional_bit_ = 8;
+};
+
+std::ostream	&operator<<(std::ostream &stdout, const Fixed &value);
+
+#endif
