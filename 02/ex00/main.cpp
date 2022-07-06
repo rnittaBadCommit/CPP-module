5eb71f7640a14488d0e3835b321bddfacabab504
rnitta <ryushironitta@yahoo.co.jp>
2022-07-06 09:35:34 +0900

    modify test(main.cpp)

--- 02/ex00/main.cpp
+++ 02/ex00/main.cpp
@@ -3,14 +3,21 @@
 
 int main( void ) {
 	Fixed a;
-	Fixed b( a );
+
+	std::cout << a.getRawBits() << std::endl;
+	a.setRawBits(100);
+	std::cout << a.getRawBits() << std::endl;
+
+	std::cout << std::endl;
+
+	Fixed b(a);
 	Fixed c;
 
 	c = b;
 
-	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+
 
 	return 0;
 }
