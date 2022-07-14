d6b00bbe5b42f03ebb8936da8f833c5ea510833a
rnitta <ryushironitta@yahoo.co.jp>
2022-07-14 16:51:40 +0900

    copy ex01, add execute() to Form.*pp

---
+++ 05/ex02/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+
+# include "Form.hpp"
+
+class Form;
+
+class	Bureaucrat
+{
+	public:
+		//statics
+		static const int	lowest_grade = 150;
+		static const int	highest_grade = 1;
+
+		//canonical
+					Bureaucrat();
+					Bureaucrat(const Bureaucrat &other);
+		virtual		~Bureaucrat();
+		Bureaucrat	&operator=(const Bureaucrat &other);
+
+		//other constructors
+					Bureaucrat(const std::string name);
+					Bureaucrat(const std::string name, const int grade);
+		//move
+		void	incrementGrade();
+		void	decrementGrade();
+		void	signForm(Form &form);
+
+		//getter & setter
+		const std::string	&getName() const;
+		int					getGrade() const;
+
+
+
+	private:
+		const std::string	name_;
+		int					grade_;
+
+		class	GradeTooHighException : public std::exception
+		{
+			virtual const char *what() const throw();
+		};
+		class	GradeTooLowException : public std::exception
+		{
+			virtual const char *what() const throw();
+		};
+};
+
+std::ostream	&operator<<(std::ostream &stream, const Bureaucrat &object);
+
+#endif
---
+++ 05/ex02/Form.cpp
@@ -0,0 +1,122 @@
+#include "Form.hpp"
+
+//canonical
+
+Form::Form() : name_("form"), is_signed_(false), grade_sign_(highest_grade), grade_exe_(highest_grade)
+{
+
+}
+
+Form::Form(const Form &other) : name_(other.name_), is_signed_(other.is_signed_), grade_sign_(other.grade_sign_), grade_exe_(other.grade_exe_)
+{
+	check_range_all_grades();
+}
+
+Form::~Form()
+{
+
+}
+
+Form	&Form::operator=(const Form &other)
+{
+	//is_signed_ = other.is_signed_;
+	//do nothing
+	(void)other;
+	return (*this);
+}
+
+
+//other constructors
+Form::Form(const std::string name, const int grade_sign, const int grade_exe) : name_(name), is_signed_(false), grade_sign_(grade_sign), grade_exe_(grade_exe)
+{
+	check_range_all_grades();
+}
+
+Form::Form(const std::string name) : name_(name), is_signed_(false), grade_sign_(highest_grade), grade_exe_(highest_grade)
+{
+
+}
+
+//move
+void	Form::beSigned(const Bureaucrat &bureaucrat)
+{
+	if (bureaucrat.getGrade() <= grade_sign_)
+	{
+		is_signed_ = true;
+	}
+	else
+		throw GradeTooLowException();
+}
+
+void	Form::execute(Bureaucrat const & executor) const
+{
+	if (executor.getGrade() <= grade_exe_)
+	{
+		try
+		{
+			execute_process();
+		}
+		catch (const std::exception &e)
+		{
+			throw e;
+		}
+	}
+	else
+		throw GradeTooLowException();
+}
+
+//getter & setter
+const std::string	&Form::getName() const
+{
+	return (name_);
+}
+
+bool	Form::getIsSigned() const
+{
+	return (is_signed_);
+}
+
+int		Form::getGradeSign() const
+{
+	return (grade_sign_);
+}
+
+int		Form::getGradeExe() const
+{
+	return (grade_exe_);
+}
+
+
+//private
+void	Form::check_range_all_grades()
+{
+	check_range_grade_(grade_sign_);
+	check_range_grade_(grade_exe_);
+}
+
+void	Form::check_range_grade_(const int grade)
+{
+	if (grade < highest_grade)
+		throw GradeTooHighException();
+	else if (grade > lowest_grade)
+		throw GradeTooLowException();
+}
+
+const char	*Form::GradeTooHighException::what() const throw()
+{
+	return ("Grade Too High");
+}
+
+const char	*Form::GradeTooLowException::what() const throw()
+{
+	return ("Grade Too Low");
+}
+
+//stream operators
+std::ostream	&operator<<(std::ostream &stream, const Form &object)
+{
+	stream << object.getName() << ", is signed: " << object.getIsSigned() 
+		<< " grade to sign: " << object.getGradeSign()
+		<< " grade to execute " << object.getGradeExe();
+	return (stream);
+}
