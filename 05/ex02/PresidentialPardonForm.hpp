fd74debda467856fc8c768f7c08c574a3a8756ed
rnitta <ryushironitta@yahoo.co.jp>
2022-07-14 18:26:57 +0900

    add include, include guard

---
+++ 05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,7 @@
+#ifndef PRESIDENTIAL_PARDON_FORM_HPP
+# define PRESIDENTIAL_PARDON_FORM_HPP
+
+# include "Form.hpp"
+
+
+#endif
---
+++ 05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,7 @@
+#ifndef ROBOTOMY_REQUEST_FORM_HPP
+# define ROBOTOMY_REQUEST_FORM_HPP
+
+# include "Form.hpp"
+
+
+#endif
