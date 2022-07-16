29d746285d6ab7be2b586a4b0f5e19e4d61830e5
rnitta <ryushironitta@yahoo.co.jp>
2022-07-16 12:45:01 +0900

    display char, int

---
+++ 06/ex00/TypeConversion.cpp
@@ -0,0 +1,97 @@
+#include "TypeConversion.hpp"
+
+const std::string TypeConversion::MESSAGE_IMPOSSIBLE = "impossible";
+const std::string TypeConversion::MESSAGE_NON_DISPLAYABLE = "Non displayable";
+
+TypeConversion::TypeConversion()
+: str_data_(""), is_number_(false)//, type_(TYPE_NONE)
+{
+
+}
+
+TypeConversion::TypeConversion(const TypeConversion &other)
+: str_data_(other.str_data_), is_number_(other.is_number_)
+{
+
+}
+
+TypeConversion::~TypeConversion()
+{
+
+}
+
+TypeConversion	&TypeConversion::operator=(const TypeConversion &other)
+{
+	str_data_ = other.str_data_;
+	is_number_ = other.is_number_;
+	// type_ = other.type_;
+
+	return (*this);
+}
+
+
+//other constructors
+TypeConversion::TypeConversion(const std::string str_data)
+: str_data_(str_data), is_number_(ft::is_number(str_data))//, type_(strData_to_type(str_data))
+{
+
+}
+
+
+//move
+void	TypeConversion::displayAllType()
+{
+	displayChar();
+}
+
+void	TypeConversion::displayChar()
+{
+	std::cout << "char: ";
+
+	if (is_number_)
+	{
+		std::istringstream	iss(str_data_);
+		int					data;
+
+		iss >> data;
+		if (ft::is_valid_range_char(data))
+		{
+			if (isprint(data))
+				std::cout << static_cast<char> (data) << std::endl;
+			else
+				std::cout << MESSAGE_NON_DISPLAYABLE << std::endl;
+		}
+		else
+			std::cout << MESSAGE_IMPOSSIBLE << std::endl;
+	}
+	else
+		std::cout << MESSAGE_IMPOSSIBLE << std::endl;
+}
+
+void	TypeConversion::displayInt()
+{
+	std::cout << "int: ";
+
+	if (is_number_)
+	{
+		std::istringstream	iss(str_data_);
+		long int			data;
+
+		iss >> data;
+		if (ft::is_valid_range_int(data))
+		{
+			std::cout << static_cast<int> (data) << std::endl;
+		}
+		else
+			std::cout << MESSAGE_IMPOSSIBLE << std::endl;
+	}
+	else
+		std::cout << MESSAGE_IMPOSSIBLE << std::endl;
+}
+
+//private
+// int		TypeConversion::strData_to_type(const std::string str_data)
+// {
+// 	if ()
+// }
+
---
+++ 06/ex00/TypeConversion.hpp
@@ -0,0 +1,49 @@
+#ifndef TYPE_CONVERSION_HPP
+# define TYPE_CONVERSION_HPP
+
+# include <iostream>
+# include <sstream>
+#include "utils/ft_utils.hpp"
+
+class TypeConversion
+{
+	public:
+					TypeConversion();
+					TypeConversion(const TypeConversion &other);
+		virtual		~TypeConversion();
+		TypeConversion	&operator=(const TypeConversion &other);
+
+		//other constructors
+		TypeConversion(const std::string str_data);
+
+		//move
+		void	displayAllType();
+		void	displayChar();
+		void	displayInt();
+		void	displayFloat();
+		void	displayDouble();
+
+	private:
+		std::string		str_data_;
+		bool			is_number_;
+		static const std::string	MESSAGE_IMPOSSIBLE;
+		static const std::string	MESSAGE_NON_DISPLAYABLE;
+		// int				type_;
+
+		// enum
+		// {
+		// 	TYPE_NONE,
+		// 	TYPE_CHAR,
+		// 	TYPE_INT,
+		// 	TYPE_FLOAT,
+		// 	TYPE_DOUBLE,
+		// 	TYPE_LAST,
+		// };
+
+		// int		strData_to_type(const std::string str_data);
+
+		
+
+};
+
+#endif
