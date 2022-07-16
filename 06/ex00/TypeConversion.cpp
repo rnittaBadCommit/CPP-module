0c3d4de13429fee51d5234e0635d6915ab348121
rnitta <ryushironitta@yahoo.co.jp>
2022-07-16 17:09:37 +0900

    print float: 42->42f

--- 06/ex00/TypeConversion.cpp
+++ 06/ex00/TypeConversion.cpp
@@ -210,7 +210,7 @@ void	TypeConversion::displayFloat(const double f_value)
 	std::cout << "float: ";
 
 	if (ft::is_valid_range_float(f_value))
-		std::cout << static_cast<float> (f_value) << std::endl;
+		std::cout << static_cast<float> (f_value) << 'f' << std::endl;
 	else
 		std::cout << MESSAGE_IMPOSSIBLE << std::endl;
 }
