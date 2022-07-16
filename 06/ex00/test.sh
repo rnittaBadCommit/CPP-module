ea062929f3f0d2f41a423358956ff350c80639bb
rnitta <ryushironitta@yahoo.co.jp>
2022-07-16 17:19:33 +0900

    add test.sh

---
+++ 06/ex00/test.sh
@@ -0,0 +1,31 @@
+
+make
+
+./convert 100
+
+./convert 0
+./convert 0f
+./convert 0.0
+./convert 0.0f
+./convert -128
+./convert -129
+./convert 127
+./convert 128
+./convert 1000000000000000000000
+./convert 1000000000000000000000.0
+./convert 1000000000000000000000.0f
+./convert 1000000000000000000000000000000000000000000000.0
+./convert -1000000000000000000000000000000000000000000000.0f
+
+./convert +inf
+./convert +inff
+./convert -inf
+./convert -inff
+./convert nan
+./convert nanf
+./convert
+./convert ""
+./convert "" ""
+
+
+
