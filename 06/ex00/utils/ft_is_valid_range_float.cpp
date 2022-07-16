9d5529907f1b9eaa749d300f43c77e64616756d2
rnitta <ryushironitta@yahoo.co.jp>
2022-07-16 17:07:46 +0900

    modify float::min -> lowest

--- 06/ex00/utils/ft_is_valid_range_float.cpp
+++ 06/ex00/utils/ft_is_valid_range_float.cpp
@@ -4,7 +4,7 @@ namespace ft
 {
 	bool	is_valid_range_float(const double d)
 	{
-		if (std::numeric_limits<float>::min() <= d && d <= std::numeric_limits<float>::max())
+		if (std::numeric_limits<float>::lowest() <= d && d <= std::numeric_limits<float>::max())
 			return (true);
 		else
 			return (false);
