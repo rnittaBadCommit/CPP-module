13316083d2bead722666ac16ee2b6752aaa3eb1b
Ryushiro Nitta <rnitta@c1r13s2.42tokyo.jp>
2022-07-18 15:04:09 +0900

    OK

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -6,9 +6,18 @@
 
 int main()
 {
-	std::vector<int> test{1, 2, 3, 4, 5, 6, 7, 8, 9, 1};
+	int tmp[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 9};
+	std::vector<int> test(tmp, std::end(tmp));
 
-	std::vector<int>::iterator itr = easyfind<std::vector<int>>(test, 1);
-	std::cout << *itr << std::endl;
-	
+	try
+	{
+		std::cout << *easyfind<std::vector<int> >(test, 1) << std::endl;
+		std::cout << *std::next(easyfind<std::vector<int> >(test, 1)) << std::endl;
+		std::cout << *easyfind<std::vector<int> >(test, 9) << std::endl;
+		std::cout << *easyfind<std::vector<int> >(test, 0) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
