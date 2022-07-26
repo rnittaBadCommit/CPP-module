0b17ae4306e09af0bdbd7f970d14a9391aa2d91d
rnitta <ryushironitta@yahoo.co.jp>
2022-07-27 08:58:41 +0900

    test

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -4,113 +4,17 @@
 #include <list>
 #include "MutantStack.hpp"
 
+template <typename T>
+void	print(MutantStack<T> src)
+{
+	std::cout << "size: " << src.size() << "top: " << src.top() << std::endl;
+}
+
 int main(void)
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << "size: " << mstack.size() << std::endl;
-	std::cout << "top: " << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << "size: " << mstack.size() << std::endl;
-	std::cout << "top: " << mstack.top() << std::endl;
-
-	std::cout << "---" << std::endl;
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(42);
-	mstack.push(4242);
-	mstack.push(24);
-	mstack.push(2424);
-	mstack.push(0);
-
-	{
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
-
-		++it;
-		--it;
-
-		std::cout << "mstack content (bottom to top): " << std::endl;
-		while(it != ite)
-		{
-			std::cout << *it;
-			if (++it == ite)
-				std::cout << std::endl;
-			else
-				std::cout << ", ";
-		}
-	}
-
-	{
-		MutantStack<int>::const_iterator it = mstack.begin();
-		MutantStack<int>::const_iterator ite = mstack.end();
-
-		++it;
-		--it;
-
-		std::cout << "mstack content (bottom to top): " << std::endl;
-		while(it != ite)
-		{
-			std::cout << *it;
-			if (++it == ite)
-				std::cout << std::endl;
-			else
-				std::cout << ", ";
-		}
-	}
-
-	{
-		MutantStack<int>::reverse_iterator it = mstack.rbegin();
-		MutantStack<int>::reverse_iterator ite = mstack.rend();
-
-		++it;
-		--it;
-
-		std::cout << "mstack content (top to bottom): " << std::endl;
-		while(it != ite)
-		{
-			std::cout << *it;
-			if (++it == ite)
-				std::cout << std::endl;
-			else
-				std::cout << ", ";
-		}
-	}
-
-	{
-		MutantStack<int>::const_reverse_iterator it = mstack.rbegin();
-		MutantStack<int>::const_reverse_iterator ite = mstack.rend();
-
-		++it;
-		--it;
-
-		std::cout << "mstack content (top to bottom): " << std::endl;
-		while(it != ite)
-		{
-			std::cout << *it;
-			if (++it == ite)
-				std::cout << std::endl;
-			else
-				std::cout << ", ";
-		}
-	}
-
-	std::cout << "---" << std::endl;
-
-	{
-		std::stack<int> s(mstack);
-		MutantStack<int> st = mstack;
-
-		std::cout << "size: " << st.size() << " (" << mstack.size() << ")" << std::endl;
-		std::cout << "top: " << st.top() << " (" << mstack.top() << ")" << std::endl;
-		st.pop();
-		std::cout << "size: " << st.size() << " (" << mstack.size() << ")" << std::endl;
-		std::cout << "top: " << st.top() << " (" << mstack.top() << ")" << std::endl;
-	}
-
-	return (0);
+	MutantStack<int> test;
+	print(test);
+	for (int i = 1; i < 1 << 6; i *= 2)
+		test.push(i);
+	print(test);
 }
