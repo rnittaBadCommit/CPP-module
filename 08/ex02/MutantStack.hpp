741dd728ed6c86215850314171dbd496adf1c1da
rnitta <ryushironitta@yahoo.co.jp>
2022-07-27 08:44:15 +0900

    test

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -1,6 +1,42 @@
 #ifndef MUTANT_STACK_HPP
 # define MUTANT_STACK_HPP
 
+# include <stack>
 
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+	public:
+		//canonical
+						MutantStack() {}
+						MutantStack(const MutantStack &other)
+						{
+							*this = other;
+						}
+		virtual			~MutantStack() {}
+		MutantStack& 	&operator=(const MutantStack &other)
+						{
+							if (this == &other)
+								return *this;
+							this->std::stack<T, Container>::operator=(other);
+							return *this;
+						}
+
+		typedef typename std::stack<T, Container>::container_type::iterator iterator;
+		typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T, Container>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		iterator begin() { return this->std::stack<T, Container>::c.begin(); }
+		iterator end() { return this->std::stack<T, Container>::c.end(); }
+
+		reverse_iterator rbegin() { return this->std::stack<T, Container>::c.rbegin(); }
+		reverse_iterator rend() { return this->std::stack<T, Container>::c.rend(); }
+
+		const_iterator begin() const { return this->std::stack<T, Container>::c.begin(); }
+		const_iterator end() const { return this->std::stack<T, Container>::c.end(); }
+
+		const_reverse_iterator rbegin() const { return this->std::stack<T, Container>::c.rbegin(); }
+		const_reverse_iterator rend() const { return this->std::stack<T, Container>::c.rend(); }
+};
 
 #endif
--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -1,6 +1,116 @@
+
+#include <iostream>
+#include <stack>
+#include <list>
 #include "MutantStack.hpp"
 
-int main()
+int main(void)
 {
-	
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << "size: " << mstack.size() << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "size: " << mstack.size() << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
+
+	std::cout << "---" << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(42);
+	mstack.push(4242);
+	mstack.push(24);
+	mstack.push(2424);
+	mstack.push(0);
+
+	{
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		std::cout << "mstack content (bottom to top): " << std::endl;
+		while(it != ite)
+		{
+			std::cout << *it;
+			if (++it == ite)
+				std::cout << std::endl;
+			else
+				std::cout << ", ";
+		}
+	}
+
+	{
+		MutantStack<int>::const_iterator it = mstack.begin();
+		MutantStack<int>::const_iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		std::cout << "mstack content (bottom to top): " << std::endl;
+		while(it != ite)
+		{
+			std::cout << *it;
+			if (++it == ite)
+				std::cout << std::endl;
+			else
+				std::cout << ", ";
+		}
+	}
+
+	{
+		MutantStack<int>::reverse_iterator it = mstack.rbegin();
+		MutantStack<int>::reverse_iterator ite = mstack.rend();
+
+		++it;
+		--it;
+
+		std::cout << "mstack content (top to bottom): " << std::endl;
+		while(it != ite)
+		{
+			std::cout << *it;
+			if (++it == ite)
+				std::cout << std::endl;
+			else
+				std::cout << ", ";
+		}
+	}
+
+	{
+		MutantStack<int>::const_reverse_iterator it = mstack.rbegin();
+		MutantStack<int>::const_reverse_iterator ite = mstack.rend();
+
+		++it;
+		--it;
+
+		std::cout << "mstack content (top to bottom): " << std::endl;
+		while(it != ite)
+		{
+			std::cout << *it;
+			if (++it == ite)
+				std::cout << std::endl;
+			else
+				std::cout << ", ";
+		}
+	}
+
+	std::cout << "---" << std::endl;
+
+	{
+		std::stack<int> s(mstack);
+		MutantStack<int> st = mstack;
+
+		std::cout << "size: " << st.size() << " (" << mstack.size() << ")" << std::endl;
+		std::cout << "top: " << st.top() << " (" << mstack.top() << ")" << std::endl;
+		st.pop();
+		std::cout << "size: " << st.size() << " (" << mstack.size() << ")" << std::endl;
+		std::cout << "top: " << st.top() << " (" << mstack.top() << ")" << std::endl;
+	}
+
+	return (0);
 }
