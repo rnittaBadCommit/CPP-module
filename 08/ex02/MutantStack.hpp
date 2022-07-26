26ace28b8d8a369b56fefc2fd4059e823524b521
rnitta <ryushironitta@yahoo.co.jp>
2022-07-27 08:49:46 +0900

    test

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -26,17 +26,17 @@ class MutantStack : public std::stack<T, Container> {
 		typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
 		typedef typename std::stack<T, Container>::container_type::const_reverse_iterator const_reverse_iterator;
 
-		iterator begin() { return (c.begin()); }
-		iterator end() { return (c.end()); }
+		iterator begin() { return (this->c.begin()); }
+		iterator end() { return (this->c.end()); }
 
-		reverse_iterator rbegin() { return (c.rbegin()); }
-		reverse_iterator rend() { return (c.rend()); }
+		reverse_iterator rbegin() { return (this->c.rbegin()); }
+		reverse_iterator rend() { return (this->c.rend()); }
 
-		const_iterator begin() const { return (c.begin()); }
-		const_iterator end() const { return (c.end()); }
+		const_iterator begin() const { return (this->c.begin()); }
+		const_iterator end() const { return (this->c.end()); }
 
-		const_reverse_iterator rbegin() const { return (c.rbegin()); }
-		const_reverse_iterator rend() const { return (c.rend()); }
+		const_reverse_iterator rbegin() const { return (this->c.rbegin()); }
+		const_reverse_iterator rend() const { return (this->c.rend()); }
 };
 
 #endif
