8a97fd8087abe7398d6eececdde0677348db6cfe
Ryushiro Nitta <rnitta@c1r13s2.42tokyo.jp>
2022-07-27 09:07:42 +0900

    add test

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -7,14 +7,17 @@
 template <typename T>
 void	print(MutantStack<T> src)
 {
-	std::cout << "size: " << src.size() << "top: " << src.top() << std::endl;
+	std::cout << "size: " << src.size() << ", top: " << src.top() << std::endl;
 }
 
 int main(void)
 {
 	MutantStack<int> test;
-	print(test);
+	std::cout << "size: " << test.size() << std::endl;
+//	print(test);
 	for (int i = 1; i < 1 << 6; i *= 2)
+	{
 		test.push(i);
-	print(test);
+		print(test);
+	}
 }
