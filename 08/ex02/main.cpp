d5182f32d03bb56f2fd0c208488f413307b179e1
rnitta <ryushironitta@yahoo.co.jp>
2022-07-27 09:29:02 +0900

    add test

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -4,12 +4,6 @@
 #include <list>
 #include "MutantStack.hpp"
 
-template <typename T>
-void	print(MutantStack<T> src)
-{
-	std::cout << "size: " << src.size() << ", top: " << src.top() << std::endl;
-}
-
 int main(void)
 {
 	MutantStack<int> test;
@@ -18,6 +12,65 @@ int main(void)
 	for (int i = 1; i < 1 << 6; i *= 2)
 	{
 		test.push(i);
-		print(test);
+		std::cout << "size: " << test.size() << ", top: " << test.top() << std::endl;
+	}
+
+	{
+		MutantStack<int>::iterator begin = test.begin();
+		MutantStack<int>::iterator end = test.end();
+
+		while (begin != end)
+		{
+			std::cout <<*begin << " ";
+			begin++;
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "========== copy constructor ==========" << std::endl;
+	MutantStack<int> copy = test;
+	{
+		MutantStack<int>::iterator begin = copy.begin();
+		MutantStack<int>::iterator end = copy.end();
+
+		while (begin != end)
+		{
+			std::cout <<*begin << " ";
+			begin++;
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "========== copy assignment operator ==========" << std::endl;
+	MutantStack<int> test2;
+	test2.push(42);
+	test2.push(43);
+	test2.push(44);
+	test2 = test;
+		
+	{
+		MutantStack<int>::iterator begin = test2.begin();
+		MutantStack<int>::iterator end = test2.end();
+
+		while (begin != end)
+		{
+			std::cout <<*begin << " ";
+			begin++;
+		}
+		std::cout << std::endl;
+	}
+
+	MutantStack<int> tmp;
+	test = tmp;
+	{
+		MutantStack<int>::iterator begin = test.begin();
+		MutantStack<int>::iterator end = test.end();
+
+		while (begin != end)
+		{
+			std::cout <<*begin << " ";
+			begin++;
+		}
+		std::cout << std::endl;
 	}
 }
